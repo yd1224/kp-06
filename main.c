828e772c5268558a2d74953c62ae78a803c7d600
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-19 16:56:21 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+double getInput(const char *prompt);
+bool isScientificNotation(const char *input);
+int main()
+{
+    float **a;
+    float *b;
+    int n;
+
+    while (1)
+    {
+        n = getInput("Enter the number of strings: ");
+        b = (float *)malloc(n * sizeof(float));
+        a = (float **)calloc(n, sizeof(float *));
+    }
+}
+
+double getInput(const char *prompt)
+{
+    char input[15];
+    double number;
+
+    do
+    {
+        printf("%s", prompt);
+        scanf("%s", input);
+
+        int invalidInput = 0;
+        for (int i = 0; input[i]; i++)
+        {
+            if (isdigit(input[i]) == 0 && input[i] != '.' && input[i] != '-' && !isScientificNotation(input))
+            {
+                printf("\nYour input is invalid\n");
+                invalidInput = 1;
+                break;
+            }
+        }
+
+        if (!invalidInput)
+        {
+            number = atof(input);
+            break;
+        }
+    } while (1);
+
+    return number;
+}
+
+bool isScientificNotation(const char *input)
+{
+    int len = strlen(input);
+    int eCount = 0;
+    int digitsBeforeE = 0;
+    int digitsAfterE = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        char c = input[i];
+        if (c == 'e' || c == 'E')
+        {
+            eCount++;
+        }
+        else if (isdigit(c) && eCount == 0)
+        {
+            digitsBeforeE++;
+        }
+        else if (isdigit(c) && eCount == 1)
+        {
+            digitsAfterE++;
+        }
+    }
+
+    return (eCount == 1) && (digitsBeforeE > 0) && (digitsAfterE > 0);
+}
